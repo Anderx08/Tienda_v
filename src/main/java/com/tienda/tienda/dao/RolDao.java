226469917f8f9772140c99685a6aeb35b9fce78d
Anderx08 <andeespi08@gmail.com>
2023-12-01 11:49:35 -0600

    Practica 11

--- src/main/java/com/tienda/tienda/dao/RolDao.java
+++ src/main/java/com/tienda/tienda/dao/RolDao.java
@@ -3,15 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.tienda.tienda.dao;
+import com.tienda.tienda.domain.Rol;
 import com.tienda.tienda.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
-    Usuario findByUsername(String username);
+public interface RolDao extends JpaRepository<Usuario, Long> {    
+
+    public void save(Rol rol);
     
-    Usuario findByUsernameAndPassword(String username, String Password);
-
-    Usuario findByUsernameOrCorreo(String username, String correo);
-
-    boolean existsByUsernameOrCorreo(String username, String correo);
 }
